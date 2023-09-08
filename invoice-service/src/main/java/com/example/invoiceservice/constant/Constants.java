4091dd99727d3a20498f23d49cf1b70c2fdd45c7
Anishka2504 <annadrozdovskaya04@gmail.com>
2023-09-08 15:57:33 +0300

    invoice service simple upload file through controller

---
+++ invoice-service/src/main/java/com/example/invoiceservice/constant/Constants.java
@@ -0,0 +1,7 @@
+package com.example.invoiceservice.constant;
+
+public class Constants {
+
+    public static final String ROOT_PATH = "/home/user/bin/projects/work/a-system-invoice-system/invoice-service/";
+    public static final String DIRECTORY_FOR_UPLOADED_FILES = "loaded";
+}
